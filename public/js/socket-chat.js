a7b2c0b1b5e2558d665b5883df4ce7c22032abe0
José Posada <josedanielpm@gmail.com>
2018-12-15 21:42:00 -0500

    Chat v1.0.0

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,25 +1,24 @@
 var socket = io();
 
+var params = new URLSearchParams(window.location.search);
 
-var params = new URLSearchParams( window.location.search );
-
-if ( !params.has('nombre') || !params.has('sala') ) {
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
-    sala: params.get('sala'),
+    sala: params.get('sala')
 };
 
 
-socket.on('connect', function() {
 
+socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function ( resp ) {
-        console.log( 'Usuarios conectados: ', resp );
+    socket.emit('entrarChat', usuario, function(resp) {
+        renderizarUsuarios(resp);
     });
 
 });
@@ -32,30 +31,21 @@ socket.on('disconnect', function() {
 });
 
 
-// Enviar información
-// socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
-//     mensaje: 'Hola Mundo'
-// }, function(resp) {
-//     console.log('respuesta server: ', resp);
-// });
-
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
-// Mensajes privado
-socket.on('mensajePrivado', function (mensaje) {
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
     console.log('Mensaje Privado:', mensaje);
+
 });
